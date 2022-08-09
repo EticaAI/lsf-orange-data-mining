dee7fa59aa9e92e801cffda37ec7f9229dc0cbbc
Emerson Rocha <rocha@ieee.org>
2022-08-09 20:02:33 -0300

    999999999/0/download-test-datasets.sh created

---
+++ 999999999/0/download-test-datasets.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  0_9.sh
+#
+#         USAGE:  ./999999999/0/download-test-datasets.sh
+#
+#   DESCRIPTION:  ---
+#
+#       OPTIONS:  ---
+#
+#  REQUIREMENTS:  - Bash shell (or better)
+#                 - curl
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Emerson Rocha <rocha[at]ieee.org>
+#       COMPANY:  EticaAI
+#       LICENSE:  Public Domain dedication or Zero-Clause BSD
+#                 SPDX-License-Identifier: Unlicense OR 0BSD
+#       VERSION:  v1.0
+#       CREATED:  2022-01-10 00:42 UTC
+#      REVISION:  ---
+#===============================================================================
+# Comment next line if not want to stop on first error
+set -e
+
+ROOTDIR="$(pwd)"
+
+#######################################
+# Download file to /999999/0/file-name.ext
+#
+# Globals:
+#   ROOTDIR
+# Arguments:
+#   filename
+#   iri
+# Outputs:
+#
+#######################################
+download_to_temp_if_need() {
+  filename="$1"
+  iri="${2}"
+
+  objectivum_archivum_temporarium="$ROOTDIR/999999/0/${filename}"
+
+  if [ -f "$objectivum_archivum_temporarium" ]; then
+    echo "Already cached [$objectivum_archivum_temporarium]"
+    return 0
+  else
+    echo "Not cached. Downloading now... [$objectivum_archivum_temporarium] [$iri]"
+  fi
+
+  curl --compressed --silent --show-error \
+    -get "$iri" \
+    --output "$objectivum_archivum_temporarium"
+}
+
+download_to_temp_if_need \
+  "1603_16_1_0.no1.bcp47.csv" \
+  "https://raw.githubusercontent.com/MDCIII/1603_16_1/main/1603/16/1/0/1603_16_1_0.no1.bcp47.csv"
+
+download_to_temp_if_need \
+  "1603_9966_1_0.no1.bcp47.csv" \
+  "https://raw.githubusercontent.com/MDCIII/1603_9966_1/main/1603/9966/1/0/1603_9966_1_0.no1.bcp47.csv"
+
+# @see https://docs.google.com/spreadsheets/d/1NjSI2LaS3SqbgYc0HdD8oIb7lofGtiHgoKKATCpwVdY/edit#gid=1088874596
+# @see https://proxy.hxlstandard.org/data/edit?dest=data_edit&filter01=cut&cut-skip-untagged01=on&strip-headers=on&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1NjSI2LaS3SqbgYc0HdD8oIb7lofGtiHgoKKATCpwVdY%2Fedit%23gid%3D1088874596
+# COD-AB-001 = UN M49 001 = World level
+download_to_temp_if_need \
+  "unocha-cod-ab-001.hxl.csv" \
+  "https://proxy.hxlstandard.org/data.csv?dest=data_edit&filter01=cut&cut-skip-untagged01=on&strip-headers=on&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1NjSI2LaS3SqbgYc0HdD8oIb7lofGtiHgoKKATCpwVdY%2Fedit%23gid%3D1088874596"
--- README.md
+++ README.md
@@ -18,12 +18,34 @@ pip install orange3-text
 
 ```
 
+### Download caches (optional)
+
+```bash
+./999999999/0/download-test-datasets.sh
+```
+
 ### Running Orange3
 
 ```bash
 orange-canvas
 ```
 
+<!--
+ ## Internal notes
+
+- https://stackoverflow.com/questions/tagged/orange
+- https://datascience.stackexchange.com/questions/24781/use-feature-constructor-in-orange-to-extract-number-from-string
+- https://orange3.readthedocs.io/projects/orange-visual-programming/en/latest/widgets/data/featureconstructor.html
+
+
+- UN OCHA / Country and Territory
+  - https://proxy.hxlstandard.org/data.csv?dest=data_edit&filter01=cut&cut-skip-untagged01=on&strip-headers=on&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1NjSI2LaS3SqbgYc0HdD8oIb7lofGtiHgoKKATCpwVdY%2Fedit%23gid%3D1088874596
+- 1603_16_1_0
+  - https://raw.githubusercontent.com/MDCIII/1603_16_1/main/1603/16/1/0/1603_16_1_0.no1.tm.hxl.csv
+- 1603_9966_1_0
+  - https://raw.githubusercontent.com/MDCIII/1603_9966_1/main/1603/9966/1/0/1603_9966_1_0.no1.tm.hxl.csv
+
+-->
 
 # License
 
