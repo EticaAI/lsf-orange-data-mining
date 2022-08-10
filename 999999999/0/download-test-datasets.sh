8ac47ba3b289336621f62bc0246f9d08d5bb150e
Emerson Rocha <rocha@ieee.org>
2022-08-10 20:22:56 -0300

    download-test-datasets.sh: internal functions for data not RDF-compatible yet

--- 999999999/0/download-test-datasets.sh
+++ 999999999/0/download-test-datasets.sh
@@ -26,6 +26,8 @@ set -e
 
 ROOTDIR="$(pwd)"
 
+#### Functions _________________________________________________________________
+
 #######################################
 # Download file to /999999/0/file-name.ext
 #
@@ -55,6 +57,8 @@ download_to_temp_if_need() {
     --output "$objectivum_archivum_temporarium"
 }
 
+#### Main ______________________________________________________________________
+
 download_to_temp_if_need \
   "1603_16_1_0.no1.bcp47.csv" \
   "https://raw.githubusercontent.com/MDCIII/1603_16_1/main/1603/16/1/0/1603_16_1_0.no1.bcp47.csv"
@@ -69,3 +73,53 @@ download_to_temp_if_need \
 download_to_temp_if_need \
   "unocha-cod-ab-001.hxl.csv" \
   "https://proxy.hxlstandard.org/data.csv?dest=data_edit&filter01=cut&cut-skip-untagged01=on&strip-headers=on&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1NjSI2LaS3SqbgYc0HdD8oIb7lofGtiHgoKKATCpwVdY%2Fedit%23gid%3D1088874596"
+
+#### Done ______________________________________________________________________
+# After here is just internal testing, mostly non-public files yet
+# The next line exit
+
+exit 0
+
+# - These scripts are from:
+#   - https://github.com/EticaAI/lexicographi-sine-finibus
+# - They use as base:
+#   - /workspace/git/EticaAI/lexicographi-sine-finibus/officina
+
+ROOTDIR_OLD="$ROOTDIR"
+ROOTDIR_NEW="/workspace/git/EticaAI/lexicographi-sine-finibus/officina"
+TEMPDIR="${ROOTDIR_OLD}/999999/0"
+
+set -x
+
+cd "${ROOTDIR_NEW}" || exit 1
+
+pwd
+
+"${ROOTDIR_NEW}/999999999/0/999999999_521850.py" \
+  --methodus-fonti=worldbank \
+  --methodus=aid-effectiveness \
+  --objectivum-formato=csv \
+  >"${TEMPDIR}/worldbank_aid-effectiveness.csv"
+
+"${ROOTDIR_NEW}/999999999/0/999999999_521850.py" \
+  --methodus-fonti=worldbank \
+  --methodus=poverty \
+  --objectivum-formato=csv \
+  >"${TEMPDIR}/worldbank_poverty.csv"
+
+"${ROOTDIR_NEW}/999999999/0/999999999_521850.py" \
+  --methodus-fonti=worldbank \
+  --methodus=poverty \
+  --objectivum-formato=csv \
+  >"${TEMPDIR}/worldbank_climate-change.csv"
+
+set +x
+cd "$ROOTDIR_OLD" || exit 0
+
+
+
+# - https://google.github.io/styleguide/
+#   - https://google.github.io/styleguide/xmlstyle.html
+#     - "Documents MAY be pretty-printed using 2-space indentation for
+#       child elements."
+# xmllint --format data-etl.temp.ows > data-etl__v2.temp.ows
